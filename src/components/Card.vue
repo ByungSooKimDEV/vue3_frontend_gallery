62b7c7aa77fbc336f6edad9ef413f7c9c1e3a883
ByungSooKimDEV <byungsoo.dev@gmail.com>
2023-05-30 08:18:07 +0900

    "장바구니 기능 구현2"

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- <div class="card shadow-sm">
+    <!-- <div class="card shadow-sm">
     <span class="img" :style="{ backgroundImage: `url(${item.imgPath})` }" />
 
     <div class="card-body">
@@ -23,53 +23,54 @@
       </div>
     </div>
   </div> -->
-  <div class="card shadow-sm">
-    <img :src="item.imgPath" />
-    <div class="card-body">
-      <p class="card-text">
-        <span>{{ item.name }} &nbsp;</span>
-        <span class="dicount badge bg-danger"> {{ item.discountPer }}% </span>
-      </p>
-      <div class="d-flex justify-content-between align-items-center">
-        <button class="btn btn-primary">구입하기</button>
-        <small class="price text-muted">
-          {{ lib.getNumberFormatted(item.price) }}원
-        </small>
-        <small class="dicount text-danger">
-          {{
-            lib.getNumberFormatted(
-              item.price - (item.price * item.discountPer) / 100
-            )
-          }}원
-        </small>
-      </div>
+    <div class="card shadow-sm">
+        <img :src="item.imgPath" />
+        <div class="card-body">
+            <p class="card-text">
+                <span>{{ item.name }} &nbsp;</span>
+                <span class="dicount badge bg-danger"> {{ item.discountPer }}% </span>
+            </p>
+            <div class="d-flex justify-content-between align-items-center">
+                <button class="btn btn-primary" @click="addToCart(item.id)">
+                    <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                </button>
+                <small class="price text-muted"> {{ lib.getNumberFormatted(item.price) }}원 </small>
+                <small class="dicount text-danger"> {{ lib.getNumberFormatted(item.price - (item.price * item.discountPer) / 100) }}원 </small>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import lib from "@/scripts/lib";
+import lib from '@/scripts/lib';
+import axios from 'axios';
 
 export default {
-  name: "Card",
-  props: {
-    item: Object,
-  },
-  setup() {
-    return { lib };
-  },
+    name: 'Card',
+    props: {
+        item: Object,
+    },
+    setup() {
+        const addToCart = (itemId) => {
+            axios.post(`/api/cart/items/${itemId}`).then(() => {
+                console.log('success');
+            });
+        };
+
+        return { lib, addToCart };
+    },
 };
 </script>
 
 <style scoped>
 .card .img {
-  display: inline-block;
-  width: 100%;
-  height: 250px;
-  background-size: cover;
+    display: inline-block;
+    width: 100%;
+    height: 250px;
+    background-size: cover;
 }
 
 .card .card-body .price {
-  text-decoration: line-through;
+    text-decoration: line-through;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -21,7 +21,7 @@
             </div>
             <div class="navbar navbar-dark bg-dark shadow-sm">
                 <div class="container">
-                    <a href="#" class="navbar-brand d-flex align-items-center">
+                    <router-link to="/" class="navbar-brand d-flex align-items-center">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             width="20"
@@ -38,8 +38,11 @@
                             <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
                             <circle cx="12" cy="13" r="4" />
                         </svg>
-                        <strong>Album</strong>
-                    </a>
+                        <strong>Gallery</strong>
+                    </router-link>
+                    <router-link to="/cart" class="cart btn">
+                        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                    </router-link>
                     <button
                         class="navbar-toggler"
                         type="button"
@@ -74,4 +77,9 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+header .navbar .cart {
+    margin-left: auto;
+    color: #fff;
+}
+</style>
